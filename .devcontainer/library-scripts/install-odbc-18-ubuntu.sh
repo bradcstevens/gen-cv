abad7369ad7f8c9bded4f7ec7b8b999b21d6d901
bradcstevens <brad@bradcstevens.com>
2024-05-20 16:07:51 -0700

    updated devcontainer configs to working build with node, python, and swa

--- .devcontainer/library-scripts/install-odbc-18-ubuntu.sh
+++ .devcontainer/library-scripts/install-odbc-18-ubuntu.sh
@@ -8,11 +8,11 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trust
 
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+ACCEPT_EULA=Y apt-get install -y mssql-tools18
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev
